59a09d85cb9589def28de89102808448e5e59153
JowenPang <jowenpg@gmail.com>
2021-05-10 21:32:43 +0800

    Jowen's commit

---
+++ src/Graph.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+
+public class Graph {
+    LinkedList<Node> nodeList;
+
+
+    public void dfs(){
+
+    }
+
+    public void bfs(){
+
+    }
+
+    public void greedySearch(){
+
+    }
+
+    public void MCTSearch(){
+
+    }
+}
